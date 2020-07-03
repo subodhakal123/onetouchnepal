92136ab19b102228c122c3ea62f6d3d143c30b72
Subodh Dhakal <subodhakal123@gmail.com>
2020-07-03 13:11:26 +0545

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(".carousel").carousel({interval: 2000});
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+$("#loginModalToggle").click(function(){
+    $("#loginModal").modal('toggle');
+});
+$("#reserveModalToggle").click(function(){
+    $("#Reserve").modal('toggle');
+});
